5f7a2dc58f215b215d031e278a4d4feeb1781536
Brandon <maier@iastate.edu>
2014-09-13 20:48:41 -0500

    More AddBathoom buttons

--- BathroomRater/src/com/bathroomrater/AddBathroom.java
+++ BathroomRater/src/com/bathroomrater/AddBathroom.java
@@ -1,16 +1,30 @@
 package com.bathroomrater;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class AddBathroom extends ActionBarActivity {
+public class AddBathroom extends ActionBarActivity implements OnItemSelectedListener {
+	private Spinner buildingSpinner;
+	private Spinner floorsSpinner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_bathroom);
+		
+		setupBuildings();
+		setupFloors();
+		
+		
 	}
 
 	@Override
@@ -30,5 +44,65 @@ public class AddBathroom extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void setupBuildings() {
+		//Fetch buildings from Firebase (temporary)
+		ArrayList<String> buildings = new ArrayList<String>();
+		buildings.add("Carver");
+		buildings.add("Coover");
+		buildings.add("Hoover");
+		
+		buildingSpinner = (Spinner) findViewById(R.id.buildings_spinner);
+		
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				this,
+				android.R.layout.simple_spinner_item,
+				buildings);
+		
+		buildingSpinner.setAdapter(adapter);
+		
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		buildingSpinner.setAdapter(adapter);
+	}
+	
+	private void setupFloors() {
+		//Fetch buildings from Firebase (temporary)
+		ArrayList<String> floors = new ArrayList<String>();
+		floors.add("1");
+		floors.add("2");
+		floors.add("3");
+		floors.add("4");
+		
+		floorsSpinner = (Spinner) findViewById(R.id.floors_spinner);
+		
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				this,
+				android.R.layout.simple_spinner_item,
+				floors);
+		
+		floorsSpinner.setAdapter(adapter);
+		
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		floorsSpinner.setAdapter(adapter);
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View arg1, int arg2,
+			long arg3) {
+		//Blank I guess
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }

5543402f757dc20a5d5956b4a3f7088329f4cf9b
Carhugar1 <Carhugar1@live.com>
2014-09-13 20:23:57 -0500

    updated

--- BathroomRater/src/com/bathroomrater/MapActivity.java
+++ BathroomRater/src/com/bathroomrater/MapActivity.java
@@ -1,36 +1,56 @@
 package com.bathroomrater;
 
+import java.util.ArrayList;
+
 import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.model.*;
 
 import android.support.v7.app.ActionBarActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MapActivity extends ActionBarActivity {
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 		
+		// users picture
+		int userImage = R.drawable.ic_launcher;
+		// Bathroom Picture
+		int bathroomImage = R.drawable.ic_launcher;
+		
 		// got to get all the data from the somewhere
 		// TODO:
+		int numb = 1;
 		
+		final ArrayList<MarkerOptions> bathrooms = new ArrayList<MarkerOptions>();
 		
-		// get the dropdown floor selection
-		Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
-		String[] items = new String[]{"1", "2", "three"};
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
-		dropdown.setAdapter(adapter);
-		// dropdown.setOnClickListener(null);
+		 for (int i = 0; i < numb; i++) {
+			 
+			 double lat = 1, lng = 1;
+			 
+			 bathrooms.add(new MarkerOptions()
+			 .icon(BitmapDescriptorFactory.fromResource(bathroomImage)) // picture for the bathroom
+             .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+             .position(new LatLng(lat, lng))
+        	 .title("Bathroom name")
+             .snippet("Avg: " + 5)
+					 );
+		 }
 		
 		// get the Google Map
 		GoogleMap map = ((MapFragment) getFragmentManager()
@@ -45,27 +65,46 @@ public class MapActivity extends ActionBarActivity {
         Location location = locationManager.getLastKnownLocation(
         		locationManager.getBestProvider(criteria, false));
         
-        // make sure its not empty
+        // make sure your location isn't empty
         if (location != null) {
         	
         	// make the person marker
-        	 MarkerOptions user = new MarkerOptions();
-             user.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)); // picture ic_launcher
-             user.anchor(0.0f, 1.0f); // Anchors the marker on the bottom left
-             user.position(new LatLng(location.getLatitude(), location.getLongitude()));
-        	 
+        	 MarkerOptions user = new MarkerOptions()
+             .icon(BitmapDescriptorFactory.fromResource(userImage)) // picture for the user
+             .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+             .position(new LatLng(location.getLatitude(), location.getLongitude()))
+        	 .title("You")
+             .snippet("Your position");
+             
         	 // add the markers
              map.addMarker(user);
+             for (int j = 0; j < bathrooms.size(); j++) {
+            	 map.addMarker(bathrooms.get(j));
+             }
+             
         	 
         	 // move to the person
              map.moveCamera(CameraUpdateFactory.newLatLngZoom(
                      new LatLng(location.getLatitude(), location.getLongitude()), 19));
              
+             // Map onclick stuff
+             map.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+
+				@Override
+				public void onInfoWindowClick(Marker arg0) {
+					if (arg0.getTitle().compareTo("You") != 0) { // nothing happens on the user
+					Intent intent = new Intent(this, );
+					intent.putExtra("BathroomName", arg0.getTitle());
+					}
+				}
+            	 
+             });
+             
              
         }       
         
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
